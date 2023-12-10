7a7befb614c1efc074f76d24af66ce1e387d5481
Kristina <monari1213@gmail.com>
2023-12-10 17:03:05 -0500

    everything is a bitch

--- home_tk_code.py
+++ home_tk_code.py
@@ -1,36 +1,52 @@
 import tkinter as tk
-import many_page as many
-import home_functions as home
+import list_page_tk_code as list
 
-#creates the window
-home_page = tk.Tk()
+def read_user_lists(file_name):
+    new_file = open(f"{file_name}.txt", "r")
+    lines = new_file.readlines()
+    return lines
 
-list_num_list = []
-task_num_list = []
+lists = read_user_lists("user_lists")
+print(lists[0][:-1])
 
-#sets the size of the page and title
-home_page.geometry("1440x1024")
-home_page.title(" home_page")
-
-#labels the window 
-user_title = tk.Label(home_page, text="To-do List", font=("times new roman", 30, "bold")).pack()
-
-#creates the new list button and places it
-new_list_button = tk.Button(home_page, text="+", relief="raised" ,font=("times new roman", 30, "bold"), command = home.create_new_list).pack()
-
-#the lists buttons and their corresponding tasks need to be adjusted for all the different possible files
-
-lists = many.read_user_lists("user_lists.txt")
-print(lists)
-    
-for i in range(len(lists)):
-    list_num_list.append(f"{lists[i]}")
-    list_num_list[-1] = tk.Button(home_page, text= list_num_list[-1 ], relief="raised", font=("times new roman", 20)).pack()
-    # tasks = ["task1", "task2"]#read.read_user_listsread_user_lists(list_num_list[-1])
-    # for j in range(len(tasks)):
-    #     task_num_list.append(f"{tasks[j]}")
-    #     task_num_list[-1] = tk.Label(user_page, text=(task_num_list[-1]), relief="ridge", font=("times new roman", 25)) 
+def open_list(list_name):
+    fr_list = str(list_name[:-1])
+    try:
+        lines = read_user_lists(f"{fr_list}.txt")
+        print(lines, list_name)
+        list.open_list(list_name, lines)
+    except: 
+        print("u suck")
+        print(fr_list)
         
 
-#opens the page
-home_page.mainloop()
+def create_new_list():
+    new_file = open("new_list.txt", "a+")
+    
+    with open("user_lists.txt", "a") as lists:
+        lists.write("\nnew_list")
+
+def open_home():
+    #creates the window
+    home_page = tk.Tk()
+
+    list_num_list = []
+    task_num_list = []
+
+    #sets the size of the page and title
+    home_page.geometry("1440x1024")
+    home_page.title(" home_page")
+
+    #labels the window 
+    user_title = tk.Label(home_page, text="To-do List", font=("times new roman", 30, "bold")).pack()
+
+    #creates the new list button and places it
+    new_list_button = tk.Button(home_page, text="+", relief="raised" ,font=("times new roman", 30, "bold"), command = create_new_list).pack()
+        
+    for i in range(len(lists)):
+        list_num_list.append(f"{lists[i]}")
+        list_num_list[-1] = tk.Button(home_page, text= list_num_list[-1 ], relief="raised", font=("times new roman", 20), command= lambda: open_list(list_num_list[-1]))
+        list_num_list[-1].pack()
+
+    #opens the page
+    home_page.mainloop()
--- list_page_tk_code.py
+++ list_page_tk_code.py
@@ -1,86 +1,32 @@
 import tkinter as tk
-import list_functions as list
+import home_tk_code as home
 
-list_page = tk.Tk()
-list_page.geometry("1440x1024")
-list_page.title("list page")
+def open_list(list_name, read_lines):
+    list_page = tk.Tk()
+    list_page.geometry("1440x1024")
+    list_page.title("list page")
+    
+    #text_box.get("1.0", tk.END)
 
-list_back_button = tk.Button(list_page, text="Back", 
-                             relief="raised" ,
-                             font=("times new roman", 60), command=list.go_back)
-list_back_button.pack(side="top", anchor="w" )
+    def back():
+        list_page.quit
+        home.open_home()
+        
+    list_back_button = tk.Button(list_page, text="Back", 
+                                    relief="raised" ,
+                                    font=("times new roman", 60), command= back)
+    list_back_button.pack()
+    #list_back_button.pack(side="top", anchor="w" )
 
-user_list = tk.Label(list_page, 
-                     text="List 1 Name", 
-                     font=("times new roman", 69, "bold")).pack()
-#user_list.pack(side="left",anchor="nw",padx=(100,0), pady=(75,0))
+    user_list = tk.Label(list_page, 
+                            text= list_name, 
+                            font=("times new roman", 69, "bold"))
+    user_list.pack()
+    #user_list.pack(side="left",anchor="nw",padx=(100,0), pady=(75,0))
 
-edit_button = tk.Button(list_page, 
-                        text="E", 
-                        relief="sunken" ,
-                        font=("times new roman", 30))
-edit_button.pack(side="right", anchor = "ne", padx = 50, pady=100)
+    text_box = tk.Text(list_page, text = read_lines)
+    text_box.pack()
 
-add_button = tk.Button(list_page, 
-                       text="A", 
-                       relief="sunken" ,
-                       font=("times new roman", 30))
-add_button.pack(side="right", anchor = "ne", padx = 50, pady=100)
-
-#                 text='<checkbox_label_1>',
-#                 #command=check_changed,
-#                 variable=checkbox_var1,
-#                 font=("times new roman", 30),
-#                 onvalue='<value_when_checked>',
-#                 offvalue='<value_when_unchecked>')
-# # https://www.pythontutorial.net/tkinter/tkinter-checkbox/
-# checkbox1.pack(side="top",anchor="w",padx=(100,0), pady=(100,0))
-
-# edit_button = tk.Button(list_page, 
-#                         text="E", 
-#                         relief="sunken" ,
-#                         font=("times new roman", 90))
-# edit_button.pack(side="right", anchor = "n", padx = (0,20))
-
-# add_button = tk.Button(list_page, 
-#                        text="A", 
-#                        relief="sunken" ,
-#                        font=("times new roman", 90))
-# add_button.pack(side="right", anchor = "n", padx =(0,20))
-
-# checkbox_var2 = tk.StringVar()
-# checkbox2 = tk.Checkbutton(list_page,
-#                 text='<checkbox_label_2>',
-#                 #command=check_changed,
-#                 variable=checkbox_var1,
-#                 font=("times new roman", 30),
-#                 onvalue='<value_when_checked>',
-#                 offvalue='<value_when_unchecked>')
-# # https://www.pythontutorial.net/tkinter/tkinter-checkbox/
-# checkbox2.pack(side="top",anchor="w",padx=(100,0), pady=(100,0))
-
-# checkbox_var3 = tk.StringVar()
-# checkbox3 = tk.Checkbutton(list_page,
-#                 text='<checkbox_label_3>',
-#                 #command=check_changed,
-#                 variable=checkbox_var1,
-#                 font=("times new roman", 30),
-#                 onvalue='<value_when_checked>',
-#                 offvalue='<value_when_unchecked>')
-# # https://www.pythontutorial.net/tkinter/tkinter-checkbox/
-# checkbox3.pack(side="top",anchor="w",padx=(100,0), pady=(100,0))
-
-# checkbox_var4 = tk.StringVar()
-# checkbox4 = tk.Checkbutton(list_page,
-#                 text='<checkbox_label_4>',
-#                 #command=check_changed,
-#                 variable=checkbox_var1,
-#                 font=("times new roman", 30),
-#                 onvalue='<value_when_checked>',
-#                 offvalue='<value_when_unchecked>')
-# # https://www.pythontutorial.net/tkinter/tkinter-checkbox/
-# checkbox4.pack(side="top",anchor="w",padx=(100,0), pady=(100,0))
-
-
-#opens the page
-#list_page.mainloop()
+    list_page.mainloop()
+    
+open_list()
